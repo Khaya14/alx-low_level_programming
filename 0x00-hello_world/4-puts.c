139002ea2e2d570837c2652c3b9e01bea0996129
Khaya14 <khayashezi14@gmail.com>
2023-10-05 11:52:21 -0700

    Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc puts("\"Programming is like building a multilingual puzzle")  
+#include <stdio.h>
+/**
+ * main - prints output using put
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
