a080d7567241d467d9b9a8df9521b97a08970368
Khaya14 <khayashezi14@gmail.com>
2023-10-23 07:41:17 -0700

    3rd Pointer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte,
+ * @s: memory area
+ * @b: constant byte
+ * @n: bytes filled
+ * Return: the pointer
+ */
+
+char *memset(char *s, char, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
+
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+3rd Pointer readme
