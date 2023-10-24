11862fbd89bfb604bfdbf097e09b2285aeadf3be
Khaya14 <khayashezi14@gmail.com>
2023-10-24 12:18:49 -0700

    3rd Pointer

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,11 +2,12 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: source adress.
- * @to: target adress.
+ * @s: source address.
+ * @to: target address.
  * Return: no return.
  */
+
 void set_string(char **s, char *to)
 {
-	**s = *to;
+	*s = to;
 }
