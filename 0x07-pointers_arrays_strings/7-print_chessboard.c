3309fa5b865b50fea483469321b741b324bc034a
Khaya14 <khayashezi14@gmail.com>
2023-10-24 11:24:22 -0700

    3rd Pointer

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 {
 	unsigned int i, m = 0;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < 8; i++)
 	{
 		if (i % 8 == 0 && i != 0)
 		{
@@ -18,5 +18,6 @@ void print_chessboard(char (*a)[8])
 		}
 		_putchar(a[i / 8][i - m]);
 	}
-	return('\n');
+	_putchar('\n');
 }
+
