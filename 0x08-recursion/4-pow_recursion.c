dd8e9cb1374cc894b94c31db88246bdcb7d15386
Khaya14 <khayashezi14@gmail.com>
2023-10-25 12:04:24 -0700

    Recursion~

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,8 +3,8 @@
 /**
  * _pow_recursion - function that returns a value that has a power
  *
- * @y: input value
- * @x: power value
+ * @y: base value
+ * @x: exponent value
  *
  * Return: power number
  */
@@ -12,7 +12,9 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-		return (-1)
+		return (-1);
+	else if (y == 0)
+		return (1);
 	else
-		return (y * pow(y++));
+		return (x * _pow_recursion(x, y - 1));
 }
