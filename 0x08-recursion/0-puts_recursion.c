8d63be0222172bec297de5f4dec6c47ae3841dac
Khaya14 <khayashezi14@gmail.com>
2023-10-25 09:49:59 -0700

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-void _puts_recursion(char *s);
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s - string
  * Return: no return.
  */
+void _puts_recursion(char *s);
 {
 	if (*s != '\0')
 	{
