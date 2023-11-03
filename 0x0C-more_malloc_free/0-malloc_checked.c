68d990c0aac83a4f54f8d6555e75396a6c6264d5
Khaya14 <khayashezi14@gmail.com>
2023-11-03 00:38:42 -0700

    More Malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory
+ *
+ * @b: integer
+ *
+ * Return: a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int b;
+
+	b = malloc_checked(sizeof(int) * size_t);
+		exit(98);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Another Malloc readme
