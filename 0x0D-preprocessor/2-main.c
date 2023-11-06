dfd2fbb27d3bdeb98593d76bf55f7f6504dbd75c
Khaya14 <khayashezi14@gmail.com>
2023-11-06 09:31:17 -0800

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#ifdef MAIN_C
+#define MAIN_C
+
+/**
+ * main - function that will preint the file that is being run.
+ *
+ * Return: Nothing
+ */
+int main(void)
+{
+	i = FILE_NAME
+	printf(i);
+	return (0);
+}
