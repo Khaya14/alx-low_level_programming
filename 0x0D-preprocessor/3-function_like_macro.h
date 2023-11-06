fa677a35c7184552bf4cff766d8d417eea4953f4
Khaya14 <khayashezi14@gmail.com>
2023-11-06 11:27:06 -0800

    Preprocessor

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,18 @@
+#ifndef FUNCTION_LIKE_MACRO_
+#define FUNCTION_LIKE_MACRO_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ABS(x)
+
+int()
+{
+	int x;
+
+	a = abs(x) * 10;
+	printf("%d\n", x);
+	return (0);
+}
+
+#endif
