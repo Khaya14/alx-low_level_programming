ee514c112d88688c29dcae03b282c08eee3b45f9
Khaya14 <khayashezi14@gmail.com>
2023-11-06 11:39:22 -0800

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,20 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_
 #define FUNCTION_LIKE_MACRO_
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#define ABS(x)
-
-int(main)
-{
-	int num;
-
-	num = abs(x) * 10;
-
-	printf("%d\n", num);
-
-	return (0);
-}
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
