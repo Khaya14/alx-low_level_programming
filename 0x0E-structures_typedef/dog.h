002fc51c43f85ca5b52f3387427c2c1048891c0c
Khaya14 <khayashezi14@gmail.com>
2023-11-07 08:21:55 -0800

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,20 @@
-#ifndef DOG_H_
-#define DOG_H_
-
-
-/**
- * dog_h: typedef for struct dog
- */
-typedef struct dog dog_h;
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct_dog - new type that defines elements
- *
- * @name: first element
- * @age: second element
- * @owner: third element
- *
- * description: struct called 'dog' stores its name, age &
- * the name of its owner
+ * dog_t - Typedef for struct dog
  */
+typedef struct dog dog_t;
 
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
 struct dog
 {
 	char *name;
@@ -31,3 +28,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
+
