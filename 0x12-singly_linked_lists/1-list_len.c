aaa293b4edd0ec9b4d86d7638aa0e8276ec51ddb
Khaya14 <khayashezi14@gmail.com>
2023-11-16 09:42:39 -0800

    S Linked

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in linked list.
+ *
+ * @h: linked list
+ *
+ * Return: numnber of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		n;
+	}
+	return (n);
+}
