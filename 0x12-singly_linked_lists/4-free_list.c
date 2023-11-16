a4648f73df9716e29031481b0bb55fc3cb627bca
Khaya14 <khayashezi14@gmail.com>
2023-11-16 12:13:50 -0800

    S Linked

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "list_h"
+
+/**
+ * free_list - frees the list (list_t).
+ *
+ * @head: head of linked list
+ *
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
