6c510756316f8804537249bd65a0775cb5b42d24
Khaya14 <khayashezi14@gmail.com>
2023-11-21 11:04:20 -0800

    More S Linked Lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (ind = 0; index && head != NULL; ind++)
 	{
-	head = head->next;
+		head = head->next;
 	}
 
 	return (head);
