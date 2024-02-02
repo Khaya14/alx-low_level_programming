9bb724d75c5b464b21ce0fc6bba698d0705ba3ed
Khaya14 <khayashezi14@gmail.com>
2024-02-02 20:49:40 +0200

    4-hash

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,13 +2,13 @@
 
 /**
  * hash_table_get - Retrieve the value associated with
- * 			a key in a hash table.
+ * a key in a hash table.
  *
  * @ht: A pointer to the hash table.
  * @key: The key to get the value of.
  *
- * Return: If the key cannot be matched - NULL. 
- * 		Otherwise - the value associated with key in ht.
+ * Return: If the key cannot be matched - NULL.
+ * Otherwise - the value associated with key in ht.
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
@@ -22,7 +22,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	idx = key_index((const unsigned char *)key, ht->size);
 	if (idx >= ht->size)
 		return (NULL);
-	
+
 	node = ht->array[idx];
 	while (node && strcmp(node->key, key) != 0)
 		node = node->next;
