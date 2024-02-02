a05e53bf3bd9166d6d5c8bb612651d6c93366ffc
Khaya14 <khayashezi14@gmail.com>
2024-02-02 21:31:49 +0200

    2-key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,7 +2,7 @@
 
 /**
  * key_index - Get the index at which a key/value pair should
- * 		be stored in array of a hash table.
+ * be stored in array of a hash table.
  * @key: The key to get the index of.
  * @size: The size of the array of the hash table.
  *
@@ -14,5 +14,5 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int djb2 = hash_djb2(key) % size;
-	return (hash_djb2(key) % size);
+	return (djb2);
 }
