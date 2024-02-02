47e8f874a4c9088e6fadf208da281411522fcc8c
Khaya14 <khayashezi14@gmail.com>
2024-02-02 19:58:31 +0200

    3-hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (value_copy == NULL)
 		return (0);
 
-	idx = key_idx((const unsigned char *)key, ht->size);
+	idx = key_index((const unsigned char *)key, ht->size);
 	for (n = idx; ht->array[n]; n++)
 	{
 		if (strcmp(ht->array[n]->key, key) == 0)
